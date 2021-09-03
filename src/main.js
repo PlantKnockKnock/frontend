4dacaf0df44e644df1eec098b19b61defcb93359
boo105 <boo10500@naver.com>
2021-09-03 14:45:39 +0900

    pusher 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
 
+
+Vue.use(require('vue-pusher'), {
+  api_key: 'da9af6249c692cb02284',
+  options: {
+      cluster: 'ap3',
+      encrypted: true,
+  }
+})
 Vue.use(VueApexCharts)
 
 Vue.component('apexchart', VueApexCharts)
